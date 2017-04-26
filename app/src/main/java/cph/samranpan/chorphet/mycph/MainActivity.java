b36d19707204633cb51bccec8837d3d8f9169b2c
chorphet <chor_ball@hotmail.com>
2017-04-26 10:48:08 +0700

    cose 5 OK

--- app/src/main/java/cph/samranpan/chorphet/mycph/MainActivity.java
+++ app/src/main/java/cph/samranpan/chorphet/mycph/MainActivity.java
@@ -2,12 +2,33 @@ package cph.samranpan.chorphet.mycph;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit การประกาศตัวแปร
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initial View การผูกตัวแปร
+        initialViwe();
+
+    }   // Main Method นี่คือ แมทตอด
+
+    private void initialViwe() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtNewRegis);
+        button = (Button) findViewById(R.id.btnLogin);
     }
-}
+
+
+}   //Main Class นี่คือ คลาสหลัก
